ff8ebddea360e495cde1e08dc2e18016f2b1ef95
Giang Nguyen <g.nguyen@vehikl.com>
2025-03-10 16:49:37 -0400

    Install Vue Calendar

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import { setupCalendar } from 'v-calendar'
 
-createApp(App)
-  .use(router)
-  .mount('#app') 
+const app = createApp(App)
+
+app.use(router)
+app.use(setupCalendar, {}) // Move this before .mount()
+
+app.mount('#app') // Now mount the app
--- src/pages/Calendar.vue
+++ src/pages/Calendar.vue
@@ -1,11 +1,11 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <h1>Calendar</h1>
+  <DatePicker v-model="date"/>
 </template>
 
-<style scoped>
+<script setup lang="ts">
+import { DatePicker } from 'v-calendar';
+import 'v-calendar/style.css';
+import {ref} from "vue";
 
-</style>
+const date = ref(new Date())
+</script>
