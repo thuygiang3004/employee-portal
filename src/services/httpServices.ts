93532bb9527eef9b2c453f2e8a6218712cf2b8b2
Giang Nguyen <g.nguyen@vehikl.com>
2025-05-08 08:20:28 -0400

    Update baseURL

--- src/services/httpServices.ts
+++ src/services/httpServices.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosResponse} from "axios";
 
 const token = localStorage.getItem('token')
-const baseURL = 'http://127.0.0.1:8000/api/'
+const baseURL = 'http://localhost/api/'
 const getRequest = async (url: string):  Promise<AxiosResponse<any, any>> => {
     return await axios.get(`${baseURL}${url}`, {
         headers: {Authorization: `Bearer ${token}`},
