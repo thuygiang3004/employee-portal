3b9dbeee25641e867e01b4bf52ac95d6e9d78950
Giang Nguyen <g.nguyen@vehikl.com>
2025-05-08 09:09:44 -0400

    Center the logo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
   <svg width="100%" height="30" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Vehiklian Logo">
-    <g transform="translate(10,10) scale(2)">
+    <g transform="translate(5,5) scale(2)">
       <circle :fill="iconColor" cx="10" cy="10" r="10"/>
       <circle fill="white" cx="10" cy="8" r="3"/>
       <rect fill="white" x="8" y="11" width="4" height="6" rx="2"/>
@@ -12,7 +12,7 @@
 defineProps({
   iconColor: {
     type: String,
-    default: 'rgb(226, 103, 0)' // Vehikl orange
+    default: 'rgb(226, 103, 0)'
   },
 })
 </script>
